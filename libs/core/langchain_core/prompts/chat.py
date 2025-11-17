085bbe7af2e45854ab2ebf09f624227feb5fb018
Bagatur <baskaryan@gmail.com>
2025-11-16 22:14:37 -0800

    fmt

--- libs/core/langchain_core/prompts/chat.py
+++ libs/core/langchain_core/prompts/chat.py
@@ -982,8 +982,17 @@ class ChatPromptTemplate(BaseChatPromptTemplate):
             "partial_variables": partial_vars,
             **kwargs,
         }
-        if template_format == "f-string" and any("." in v for v in kwargs['input_variables'] + kwargs['optional_variables'] + list(kwargs['partial_variables'].keys())):
-            raise ValueError("Invalid f-string template variable. Cannot access input object attributes (cannot have '.' in variables).")
+        if template_format == "f-string" and any(
+            "." in v
+            for v in kwargs["input_variables"]
+            + kwargs["optional_variables"]
+            + list(kwargs["partial_variables"].keys())
+        ):
+            err_msg = (
+                "Invalid f-string template variable. Cannot access input object "
+                "attributes (cannot have '.' in variables)."
+            )
+            raise ValueError(err_msg)
         cast("type[ChatPromptTemplate]", super()).__init__(messages=messages_, **kwargs)
 
     @classmethod
